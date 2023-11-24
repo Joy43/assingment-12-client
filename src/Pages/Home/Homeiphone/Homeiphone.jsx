3e942a299a8d6007328537854ea340155ad1897d
joy43 <ssjoy43@gmail.com>
2023-11-24 22:34:52 +0600

    home page product add

--- src/Pages/Home/Homeiphone/Homeiphone.jsx
+++ src/Pages/Home/Homeiphone/Homeiphone.jsx
@@ -1,12 +1,19 @@
 import useProduct from "../../../Hooks/useProduct";
+import Homeiphonecard from "./HomeIphonecaard/Homeiphonecard";
 
 
 const Homeiphone = () => {
     const[product]=useProduct()
     const iphonehome=product.filter(item=>item.category==='iPhone')
     return (
-        <div>
-            
+        <div className="grid md:grid-cols-2 gap-10">
+            {
+                iphonehome.map(item=><Homeiphonecard 
+                key={item._id}
+                item={item}>
+
+                </Homeiphonecard>)
+            }
         </div>
     );
 };
