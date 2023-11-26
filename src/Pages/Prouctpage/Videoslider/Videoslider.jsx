072949fe509958384ffcc45e3b6fc7ffc1223185
joy43 <ssjoy43@gmail.com>
2023-11-26 22:50:01 +0600

    local stroage data load

--- src/Pages/Prouctpage/Videoslider/Videoslider.jsx
+++ src/Pages/Prouctpage/Videoslider/Videoslider.jsx
@@ -3,16 +3,32 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'video-react/dist/video-react.css';
+import Vid1 from '../../.././assets/vidslide/1.mp4'
+import Vid2 from '../../.././assets/vidslide/1.mp4'
+import Vid3 from '../../.././assets/vidslide/2.mp4'
+import Vid4 from '../../.././assets/vidslide/3.mp4'
+import Vid5 from '../../.././assets/vidslide/4.mp4'
 const videos = [
     {
       id: 1,
-      src: 'https://example.com/video1.mp4',
+      src: Vid1,
     },
     {
       id: 2,
-      src: 'https://example.com/video2.mp4',
+      src: Vid2,
     },
-    // Add more videos as needed
+    {
+      id: 3,
+      src: Vid3,
+    },
+    {
+      id: 4,
+      src: Vid4,
+    },
+    {
+      id: 5,
+      src: Vid5,
+    },
   ];
 const Videoslider = () => {
     const settings = {
