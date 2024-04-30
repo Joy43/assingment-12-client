daece5a913e113927f8dd04780b07751e65290fc
joy43 <ssjoy43@gmail.com>
2024-04-30 13:12:27 +0600

    vercel deploy ready

--- src/Pages/Dashboad/payment/Cheackpayment/CheackPayment.jsx
+++ src/Pages/Dashboad/payment/Cheackpayment/CheackPayment.jsx
@@ -7,7 +7,7 @@ console.log(stripePromise);
 
 const CheackPayment = () => {
   return (
-    <div className="bg-base-300 shadow-lg  p-2 text-white">
+    <div className="bg-base-300 shadow-lg mx-auto p-2 text-white">
       <h1>Welcome to payment method</h1>
       <Elements stripe={stripePromise}>
         <CheckoutForm></CheckoutForm>
