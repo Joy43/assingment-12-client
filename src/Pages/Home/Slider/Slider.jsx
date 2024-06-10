e60beda9372afee471677d08fdf64e11213595da
joy43 <ssjoy43@gmail.com>
2024-06-10 12:23:57 +0600

    card homepage design change

--- src/Pages/Home/Slider/Slider.jsx
+++ src/Pages/Home/Slider/Slider.jsx
@@ -1,22 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const images = [
   {
-    url: 'https://i.ibb.co/JFmR5RJ/1.png',
+    url: "https://i.ibb.co/JFmR5RJ/1.png",
   },
   {
-    url: 'https://i.ibb.co/8Nt8kHx/2.png',
+    url: "https://i.ibb.co/8Nt8kHx/2.png",
   },
   {
-    url: 'https://i.ibb.co/7CXJyQ8/3.png',
+    url: "https://i.ibb.co/7CXJyQ8/3.png",
   },
   {
-    url: 'https://i.ibb.co/vcfpMY8/4.png',
+    url: "https://i.ibb.co/vcfpMY8/4.png",
   },
   {
-    url: 'https://i.ibb.co/T4GRPJJ/5.png',
-  }
- 
+    url: "https://i.ibb.co/T4GRPJJ/5.png",
+  },
 ];
 
 function Carousel() {
@@ -42,18 +41,67 @@ function Carousel() {
 
   return (
     <div>
-      <div className="carousel mt-6">
-        <button onClick={prevImage} className="prev-button">
-          &lt;
+      <div className="h-60 w-full md:h-[470px] lg:h-[540px] relative overflow-hidden">
+        {/* -----------left image----------- */}
+        <button
+          onClick={prevImage}
+          className="absolute top-1/2 left-3 z-50 flex justify-center items-center bg-white rounded-full w-6 h-6 md:w-8 md:h-8"
+        >
+          <svg
+            className="w-4 h-4 md:w-6 md:h-6 icon"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#000000"
+          >
+            <g strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fill="#0095FF"
+                d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
+              ></path>
+            </g>
+          </svg>
         </button>
         <div className="image-container">
           <div className=" absolute text-center">
             {/* Title and description elements removed */}
           </div>
-          <img src={images[currentImage].url} alt="Carousel" className="carousel-image " />
+          <img
+            src={images[currentImage].url}
+            alt="Carousel"
+            className="carousel-image min-w-full h-60 bg-black/20 sm:h-96 md:h-[540px] object-cover "
+          />
         </div>
-        <button onClick={nextImage} className="next-button">
-          &gt;
+        {/* arrow right */}
+        <button
+          onClick={nextImage}
+          className="absolute top-1/2 z-50 right-3  flex justify-center items-center bg-white rounded-full w-6 h-6 md:w-8 md:h-8"
+        >
+          <svg
+            className="w-4 h-4 md:w-6 md:h-6 icon"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#000000"
+            transform="rotate(180)"
+          >
+            <g strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fill="#0095FF"
+                d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
+              ></path>
+            </g>
+          </svg>
         </button>
       </div>
       <div></div>
